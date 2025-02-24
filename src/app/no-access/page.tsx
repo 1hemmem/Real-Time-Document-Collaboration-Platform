6d9a25c8dbb691ed669841281a83dd41807b817f
1hemmem <charenbahaeddinehemmem@gmail.com>
2025-02-24 17:25:11 +0100

    another build failuer, changed &apos;

--- src/app/no-access/page.tsx
+++ src/app/no-access/page.tsx
@@ -2,7 +2,7 @@
 
 export default function ErrorPage() {
   return <div className="flex flex-col justify-center items-center h-screen">
-    <p className="text-2xl font-bold font-mono">Sorry, you don't have access to this room</p>
+    <p className="text-2xl font-bold font-mono">Sorry, you don&apos;t have access to this room</p>
     <br />
     <button className="bg-black text-white p-2 rounded-md" onClick={() => {
       window.location.href = "/playground";
